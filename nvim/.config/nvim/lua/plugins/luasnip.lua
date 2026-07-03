3ad336cf03c6b5761c2db47968f6ca7d34b4e627
Gilberto Leon <ripflame@gmail.com>
2026-07-02 22:54:11 -0600

    chore(nvim): update plugin lockfile and fix luasnip path
    Point luasnip snippets at stdpath("config") instead of the old
    ~/.nvim-setup path, which no longer exists after the config move.

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -2,7 +2,7 @@ return {
   "L3MON4D3/LuaSnip",
   build = "make install_jsregexp",
   config = function()
-    local snippets_path = vim.fn.expand("~/.nvim-setup/nvim/snippets")
+    local snippets_path = vim.fn.stdpath("config") .. "/snippets"
     require("luasnip.loaders.from_snipmate").lazy_load({
       paths = snippets_path,
       fs_event_providers = { "libuv" }
